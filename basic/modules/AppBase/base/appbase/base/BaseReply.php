64f730695cb9d510535bc88e9909065ff182e3db
gjc117 <gjc117@126.com>
2015-12-24 14:55:30 +0800

    add merge_reply

--- basic/modules/AppBase/base/appbase/base/BaseReply.php
+++ basic/modules/AppBase/base/appbase/base/BaseReply.php
@@ -7,4 +7,17 @@ namespace app\modules\AppBase\base\appbase\base;
 use app\modules\AppBase\base\appbase\BaseAR;
 class BaseReply extends BaseAR
 {
+    public function merge_reply(&$reply_list, $tmp)
+    {
+        $aim = [];
+        foreach ($reply_list as $v) {
+            $aim[] = $v;
+            foreach ($tmp as $y) {
+                if ($v['id'] == $y['link_id']) {
+                    $aim[] = $y;
+                }
+            }
+        }
+        $reply_list=$aim;
+    }
 }
