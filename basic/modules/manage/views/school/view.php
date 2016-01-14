9422881c660ff82aeca5a3eb26a90c165495c976
gjc117 <gjc117@126.com>
2016-01-14 11:40:51 +0800

    add monitor null

--- basic/modules/manage/views/school/view.php
+++ basic/modules/manage/views/school/view.php
@@ -23,67 +23,94 @@ $this->params['breadcrumbs'][] = $this->title;
             <div class="panel-body">
                 <div class="adv-table editable-table">
                     <!-- <div class="space15"></div> -->
-                    <table class="table table-striped table-hover table-bordered" id="editable-sample" style="margin-top:20px;">
-                            <tr>
-                            	<th width="20%;">学校名称</th>
-                            	<td><span title="编辑" onclick="listTable.edit(this, 'name', 12,edit_url)"><?= $school['name'] ?></span></td>
-                            </tr>
-                            <tr>
-                            	<th width="20%;">学校码</th>
-                            	<td><?= $school['code'] ?></td>
-                            </tr>
-                            <tr>
-                            	<th width="20%;">联系电话</th>
-                            	<td><span title="编辑" onclick="listTable.edit(this, 'tel', 12,edit_url)"><?= $school['tel'] ?></span></td>
-                            </tr>
-                            <tr>
-                            	<th width="20%;">QQ</th>
-                            	<td><span title="编辑" onclick="listTable.edit(this, 'qq', 12,edit_url)"><?= $school['qq'] ?></span></td>
-                            </tr>
-                            <tr>
-                            	<th width="20%;">邮箱</th>
-                            	<td><span title="编辑" onclick="listTable.edit(this, 'email', 12,edit_url)"><?= $school['email'] ?></span></td>
-                            </tr>
-                            <tr>
-                            	<th width="20%;">地址</th>
-                            	<td><span title="编辑" onclick="listTable.edit(this, 'address', 12,edit_url)"><?= $school['address'] ?></span></td>
-                            </tr>
-                            <tr>
-                            	<th width="20%;">创建时间</th>
-                            	<td><?= $school['createtime'] ?></td>
-                            </tr>
+                    <table class="table table-striped table-hover table-bordered" id="editable-sample"
+                           style="margin-top:20px;">
+                        <tr>
+                            <th width="20%;">学校名称</th>
+                            <td><span name="edit" title="编辑"
+                                      onclick="listTable.edit(this, 'name', 12,edit_url)"><?= $school['name'] ?></span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th width="20%;">学校码</th>
+                            <td><?= $school['code'] ?></td>
+                        </tr>
+                        <tr>
+                            <th width="20%;">联系电话</th>
+                            <td><span name="edit" title="编辑"
+                                      onclick="listTable.edit(this, 'tel', 12,edit_url)"><?= $school['tel'] ?></span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th width="20%;">QQ</th>
+                            <td><span name="edit" title="编辑"
+                                      onclick="listTable.edit(this, 'qq', 12,edit_url)"><?= $school['qq'] ?></span></td>
+                        </tr>
+                        <tr>
+                            <th width="20%;">邮箱</th>
+                            <td><span name="edit" title="编辑"
+                                      onclick="listTable.edit(this, 'email', 12,edit_url)"><?= $school['email'] ?></span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th width="20%;">地址</th>
+                            <td><span name="edit" title="编辑"
+                                      onclick="listTable.edit(this, 'address', 12,edit_url)"><?= $school['address'] ?></span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th width="20%;">创建时间</th>
+                            <td><?= $school['createtime'] ?></td>
+                        </tr>
                     </table>
-                </div><!-- adv-table结束 -->
+                </div>
+                <!-- adv-table结束 -->
                 <span><mark style="color:#900;">注意：表格内部分数据点击即可编辑。</mark></span>
-            </div><!-- panel-body结束 -->
+            </div>
+            <!-- panel-body结束 -->
         </section>
 
-         <section class="panel panel-warning">
+        <section class="panel panel-warning">
             <header class="panel-heading">
                 <span>园长信息</span>
             </header>
             <div class="panel-body">
                 <div class="adv-table editable-table">
                     <div class="space15"></div>
-                    <table class="table table-striped table-hover table-bordered" id="editable-sample" style="margin-top:20px;">
-                            <tr>
-                            	<th width="20%;">园长姓名</th>
-                            	<td><span title="编辑" onclick="listTable.edit(this, 'name_zh', 12,edit_url2)"><?= $master['name_zh'] ?>
-                            		<!-- <span class="glyphicon glyphicon-edit"></span> -->
+                    <table class="table table-striped table-hover table-bordered" id="editable-sample"
+                           style="margin-top:20px;">
+                        <tr>
+                            <th width="20%;">园长姓名</th>
+                            <td><span name="edit" title="编辑"
+                                      onclick="listTable.edit(this, 'name_zh', 12,edit_url2)"><?= $master['name_zh'] ?>
+                                    <!-- <span class="glyphicon glyphicon-edit"></span> -->
                             		</span></td>
-                            </tr>
-                            <tr>
-                            	<th width="20%;">园长电话</th>
-                            	<td><span title="编辑" onclick="listTable.edit(this, 'phone', 12,edit_url2)"><?= $master['phone'] ?></span></td>
-                            </tr>
-                            <tr>
-                            	<th width="20%;">修改密码</th>
-                            	<td><span title="编辑" onclick="listTable.edit(this, 'password', 12,edit_url2)">点击修改密码</span></td>
-                            </tr>
+                        </tr>
+                        <tr>
+                            <th width="20%;">园长电话</th>
+                            <td><span name="edit" title="编辑"
+                                      onclick="listTable.edit(this, 'phone', 12,edit_url2)"><?= $master['phone'] ?></span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th width="20%;">修改密码</th>
+                            <td><span name="edit" title="编辑" onclick="listTable.edit(this, 'password', 12,edit_url2)">点击修改密码</span>
+                            </td>
+                        </tr>
                     </table>
-                </div><!-- adv-table结束 -->
+                </div>
+                <!-- adv-table结束 -->
                 <span><mark style="color:#900;">注意：表格内数据点击即可编辑。</mark></span>
-            </div><!-- panel-body结束 -->
+            </div>
+            <!-- panel-body结束 -->
         </section>
-    </div><!-- col-*结束 -->
+    </div>
+    <!-- col-*结束 -->
 </div><!-- wrapper结束 -->
+<script language="javascript">
+    $("span[name='edit']").each(function () {
+        if (!$(this).text()) {
+            $(this).text('没有信息哦!');
+        }
+    });
+</script>
