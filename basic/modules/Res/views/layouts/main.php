bea0b13ee712997c0c6b4a37305bdcbb007473a4
gjc117 <gjc117@126.com>
2016-01-24 08:15:44 +0800

    add module : Res

--- basic/modules/Res/views/layouts/main.php
+++ basic/modules/Res/views/layouts/main.php
@@ -16,19 +16,55 @@ AppAsset::register($this);
 
     <?php $this->head() ?>
 </head>
-<body class="horizontal-menu">
+<body>
 <?php
 $this->beginBody()
 ?>
 <div class="container">
-    <div class="">
-        jjjj
-        <div style="border-style:outset;">
-            <?= $content ?>
-        </div>
+    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+    	<ol class="carousel-indicators">
+    		<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    		<li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    		<li data-target="#carousel-example-generic" data-slide-to="2"></li>
+    	</ol>
+
+    	<div class="carousel-inner" role="listbox">
+    		<div class="item active">
+    			<a href="http://v.qq.com/cover/3/38uyds1h60wr7b5.html?vid=o0016m3ajom">
+    				<img src="images/res-img/dl.png" alt="First Slide">
+    			</a>
+    		</div>
+
+    		<div class="item">
+    			<a href="http://www.iqiyi.com/v_19rrnb2z9k.html?vfm=2002_2345f">
+    				<img src="images/res-img/xw.png" alt="Second Slide">
+    			</a>
+    		</div>
+
+    		<div class="item">
+    			<a href="http://www.letv.com/ptv/vplay/23832104.html?ch=2345_dh">
+    				<img src="images/res-img/dt.png" alt="Third Slide">
+    			</a>
+    		</div>
+    	</div>
+
+    	<!-- Controls -->
+    	<a href="#carousel-example-generic" class="left carousel-control" role="button" data-slide="prev">
+    		<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    		<span class="sr-only">Previous</span>
+    	</a>
+    	<a href="#carousel-example-generic" class="right carousel-control" role="button" data-slide="next">
+    		<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    		<span class="sr-only">Next</span>
+    	</a>
     </div>
 </div>
-<!-- container结束 -->
+<br>
+	<div>
+	    <?= $content ?>
+	</div>
+
+<script src="js/bootstrap.min.js"></script>
 <?php $this->endBody() ?>
 </body>
 </html>
